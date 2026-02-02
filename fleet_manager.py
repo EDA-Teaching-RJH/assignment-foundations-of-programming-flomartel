fd149b5106c0667b5f0959b290c05b1e3d7aca95
flomartel <fjm25@kent.ac.uk>
2026-02-02 13:02:08 +0000

    partB_ver1

---
+++ fleet_manager.py
@@ -0,0 +1,11 @@
+##Part B, the modular architechture 
+
+def init_database():
+    names = ["Picard", "Riker", "Data", "Worf", "La Forge"] ##names of startrek tng characters
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"] ##ranks of tng characters
+    divs = ["Command", "Command", "Operations", "Security", "Operations"] ##divisions of tng characters
+    ids = [1101, 1102, 1103, 1104, 1105] ##ids of tng characters. i made these up im not sure if they have actual ones
+
+    return names, ranks, divs, ids ##exits function and stores data
+
+
