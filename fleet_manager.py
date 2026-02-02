ef1cd59c293545ea1f1e51b06e0bd1c26c39aab1
flomartel <fjm25@kent.ac.uk>
2026-02-02 20:02:29 +0000

    partB_ver5

--- fleet_manager.py
+++ fleet_manager.py
@@ -24,13 +24,45 @@ def display_menu(user_name): ##defines the display menu. user selects from here
     opt = input("Select Option: ") ##prints select option and user inputs number
     return opt
 
-def add_member():
-        
+def add_member(names, ranks, divs, ids):
+    new_name = input("Enter new member's name: ")
+    new_rank = input("Enter new member's rank: ")
+    new_div = input("Enter new member's division: ")
+    new_id = input("Enter new member's ID number: ") ##asks user for new name,rank,division,id number
 
-def main(): ##defines main function. call everything from inside here
-        
+    valid_TNG_ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+    if new_rank not in valid_TNG_ranks:
+        print("Please enter a valid TNG rank") ## if rank given isnt one of the listed it will prompt for another rank
+        return
+
+    if new_id in ids:
+        print("Please enter a new and correct ID number") ##if id is already in use it will prompt for a different number
+        return
+
+    names.append(new_name)
+    ranks.append(new_rank)
+    divs.append(new_div)
+    ids.append(new_id) ##adds new crew member to the roster list
+    print("New crew member added.")   
+
+
+##tomorrow do def remove_member()
+
+    def main(): ##defines main function. call everything from inside here
+        names, ranks, divs, ids = init_database() ## calls init_database inside main and retuns the 4 lists
         user_name = input("What is your full name? ")
         
 
         active = True
         while active:
+             opt = display_menu(user_name)
+
+             if opt == "1":
+                 add_member(names, ranks, divs, ids)
+
+            ##elif opt 2 remove_member()
+
+
+
+
+    main()
